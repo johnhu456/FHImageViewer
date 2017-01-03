e62d65b8aab44226c8a6ce90a5b58b1e0412180d
Hu Xiang <johnhu456@163.com>
2017-01-03 16:35:25 +0800

    1、解决双击缩放的问题

--- Demo/Demo/FHImageViewerCell.m
+++ Demo/Demo/FHImageViewerCell.m
@@ -59,13 +59,9 @@
     _imageScrollView.bouncesZoom = NO;
     _imageScrollView.showsVerticalScrollIndicator = _imageScrollView.showsVerticalScrollIndicator = NO;
     _imageScrollView.clipsToBounds = YES;
-#warning todo
-    _imageScrollView.backgroundColor = [UIColor yellowColor];
     
     _imageMaskView = [[UIView alloc] initWithFrame:_imageScrollView.frame];
     _imageMaskView.clipsToBounds = YES;
-#warning todo
-    _imageMaskView.backgroundColor = [UIColor redColor];
     
     _imageView = [[UIImageView alloc] initWithFrame:_imageScrollView.frame];
     _imageView.userInteractionEnabled = YES;
@@ -73,8 +69,6 @@
     _imageView.center = _imageMaskView.center;
     _imageView.contentMode = UIViewContentModeScaleAspectFit;
     _imageView.clipsToBounds = YES;
-#warning todo
-    _imageView.backgroundColor = [UIColor redColor];
     
     [_imageScrollView addSubview:_imageMaskView];
     [_imageMaskView addSubview:_imageView];
@@ -101,8 +95,8 @@
 {
     _image = image;
     _imageView.image = image;
-    CGFloat newHeight = _imageView.frame.size.width * image.scale;
-    _imageMaskView.frame = CGRectMake(_imageView.frame.origin.x, (self.frame.size.height - newHeight)/2.f, _imageView.frame.size.width , _imageView.frame.size.width * image.scale);
+    CGFloat newHeight = _imageView.frame.size.width * image.size.height/image.size.width;
+    _imageMaskView.frame = CGRectMake(_imageView.frame.origin.x, (self.frame.size.height - newHeight)/2.f, _imageView.frame.size.width , newHeight);
     _imageView.frame = CGRectMake(0, 0, _imageMaskView.frame.size.width, _imageMaskView.frame.size.height);
     _imageScrollView.maximumZoomScale = _imageScrollView.frame.size.height/_imageView.frame.size.height;
     [_imageScrollView setZoomScale:1.f animated:NO];
@@ -123,15 +117,10 @@
     CGPoint center = [tapGR locationInView:_imageView];
     if (_enlarge) {
         [_imageScrollView zoomToRect:[self zoomRectForScale:1.f withCenter:center] animated:YES];
-//        [_imageScrollView setZoomScale:1.f animated:YES];
-//        [_imageScrollView setContentOffset:CGPointMake(center.x - _imageView.center.x, center.y - _imageView.center.y)];
         _enlarge = NO;
     }
     else{
         [_imageScrollView zoomToRect:[self zoomRectForScale:_imageScrollView.maximumZoomScale withCenter:center] animated:YES];
-        NSLog(@"%@",NSStringFromCGRect([self zoomRectForScale:2.f withCenter:center]));
-//        [_imageScrollView setContentOffset:CGPointMake(center.x - _imageView.center.x, center.y - _imageView.center.y)];
-//        [_imageScrollView setZoomScale:2.f animated:YES];
         _enlarge = YES;
     }
 }
@@ -141,6 +130,16 @@
 {
     return _imageMaskView;
 }
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    CGFloat offsetX = (scrollView.bounds.size.width > scrollView.contentSize.width)?
+    (scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5 : 0.0;
+    CGFloat offsetY = (scrollView.bounds.size.height > scrollView.contentSize.height)?
+    (scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5 : 0.0;
+    _imageMaskView.center = CGPointMake(scrollView.contentSize.width * 0.5 + offsetX,
+                                        scrollView.contentSize.height * 0.5 + offsetY);
+}
+
 
 #pragma mark - PublicMethod
 - (void)setParallaxValue:(CGFloat)value
@@ -154,36 +153,8 @@
     CGRect zoomRect;
     zoomRect.size.height =_imageView.frame.size.height/scale;
     zoomRect.size.width  =_imageView.frame.size.width/scale;
-//    zoomRect.origin.x = _imageView.frame.size.width - center.x;
-//    zoomRect.origin.y = _imageView.frame.size.height - center.y + 64.f;
-    zoomRect.origin.x = center.x - (zoomRect.size.width  /2.0);
-    zoomRect.origin.y = center.y - (zoomRect.size.height /2.0);
-//    zoomRect.origin.x = (center.x * (2 - _imageScrollView.minimumZoomScale) - (zoomRect.size.width / 2.0));
-//    zoomRect.origin.y = (center.y * (2 - _imageScrollView.minimumZoomScale) - (zoomRect.size.height / 2.0));
-//    zoomRect.origin.x = center.x/scale - (_imageScrollView.frame.size.width/2) +_imageScrollView.contentOffset.x/scale;
-//    zoomRect.origin.y = center.y/scale - (_imageScrollView.frame.size.height/2) + _imageScrollView.contentOffset.y/scale;
-    return zoomRect;
-    
-//    
-//    CGSize contentSize;
-//    contentSize.width = (_imageScrollView.contentSize.width/scale);
-//    contentSize.height = ((_imageScrollView.contentSize.height - 64)/scale);
-//    
-//    //translate the zoom point to relative to the content rect
-//    center.x = (center.x / self.bounds.size.width) * contentSize.width;
-//    center.y = (center.y / (self.bounds.size.height - 64)) * contentSize.height;
-//    
-//    //derive the size of the region to zoom to
-//    CGSize zoomSize;
-//    zoomSize.width = self.bounds.size.width / scale;
-//    zoomSize.height = (self.bounds.size.height - 64) / scale;
-//    
-//    //offset the zoom rect so the actual zoom point is in the middle of the rectangle
-//    CGRect zoomRect;
-//    zoomRect.origin.x = center.x - zoomSize.width / 2.0f;
-//    zoomRect.origin.y = center.y - zoomSize.height / 2.0f;
-//    zoomRect.size.width = zoomSize.width;
-//    zoomRect.size.height = zoomSize.height;
+    zoomRect.origin.x = center.x - (zoomRect.size.width  /scale);
+    zoomRect.origin.y = center.y - (zoomRect.size.height /scale);
     return zoomRect;
 }
 
@@ -192,16 +163,4 @@
     [super prepareForReuse];
     _imageView.transform = CGAffineTransformMakeTranslation(0, 0);
 }
-
-#pragma mark - UIGestureRecognizer
-//- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-//    return YES;
-//}
-
-//- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-//    if (otherGestureRecognizer == _imageScrollView.panGestureRecognizer) {
-//        return YES;
-//    }
-//    return YES;
-//}
 @end
